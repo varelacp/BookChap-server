c914024404f56dd82c86e64f038116aad9385754
loureiroand <124377344+loureiroand@users.noreply.github.com>
2023-07-10 21:55:00 +0100

    book model updated

--- models/Book.model.js
+++ models/Book.model.js
@@ -2,14 +2,16 @@ const { Schema, model } = require('mongoose');
 
 const bookSchema = new Schema({
   title: String,
-  author: String,
+  author: [String],
   description: String,
   availability: Boolean,
   rentedBy: { type: Schema.Types.ObjectId, ref: 'User' },
   category: String,
   imgUrl: String,
   isbn: String,
-  rating: Number
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+  rentalPrice: Number,
+  apiId: String
 });
 
 module.exports = model('Book', bookSchema);
---
+++ models/Review.model.js
@@ -0,0 +1,9 @@
+const { Schema, model } = require('mongoose');
+
+const reviewSchema = new Schema({
+  review: {
+    type: String
+  }
+});
+
+module.exports = model('Review', reviewSchema);
